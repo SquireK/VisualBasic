9cb29650c38295fef661e0ededbb558c81822592
SquireK <sebastiankaye076@rusdlearns.net>
2015-09-24 14:21:20 -0700

    Create simpleClasses.vb

---
+++ simpleClasses.vb
@@ -0,0 +1,14 @@
+Module Module1
+
+  Sub Main()
+
+    Dim myNewCar As New Car()
+    
+    myNewCar.make = "Oldsmobile"
+    myNewCar.model = "Cutlas Supreme"
+    myNewCar.year = 1986
+    myNewCar.color = "Silver"
+  
+  End Sub
+
+End Module
