5cf37ca9f5fffce62b2a18fe44533909304ff032
SquireK <sebastiankaye076@rusdlearns.net>
2015-09-24 14:48:11 -0700

    Update car.vb

--- car.vb
+++ car.vb
@@ -4,5 +4,11 @@ Public Class car
   Public Property model As String
   Public Property year As Integer
   Public Property color As String
+  
+  Public Function determineMarketValue() As Double
+
+    Return 100.00
+
+  End Function
 
 End Class
