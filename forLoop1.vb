433418cccde59ad957c71f291206d512dde5f026
SquireK <sebastiankaye076@rusdlearns.net>
2015-10-02 14:08:46 -0700

    Update forLoop1.vb

--- forLoop1.vb
+++ forLoop1.vb
@@ -28,6 +28,31 @@ Module Module1
 
         Next
 
+        Console.WriteLine()
+
+        For i = 1 To 10
+
+            For j = 1 To i
+
+                Console.Write("1")
+
+            Next
+
+            Console.WriteLine()
+
+        Next
+        For i = 9 To 1 Step -1
+
+            For j = 1 To i
+
+                Console.Write("1")
+
+            Next
+
+            Console.WriteLine()
+
+        Next
+
         Console.ReadLine()
 
     End Sub
